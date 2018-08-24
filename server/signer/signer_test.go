de69d44e32169404769734d4b7f145a3109bb433
Niall Sheridan <nsheridan@gmail.com>
2018-08-24 20:58:55 +0100

    More tests

--- server/signer/signer_test.go
+++ server/signer/signer_test.go
@@ -2,11 +2,15 @@ package signer
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"reflect"
 	"testing"
 	"time"
 
 	"github.com/nsheridan/cashier/lib"
+	"github.com/nsheridan/cashier/server/config"
 	"github.com/nsheridan/cashier/server/store"
 	"github.com/nsheridan/cashier/testdata"
 	"github.com/stripe/krl"
@@ -15,14 +19,27 @@ import (
 )
 
 var (
-	key, _ = ssh.ParsePrivateKey(testdata.Priv)
-	signer = &KeySigner{
-		ca:          key,
-		validity:    12 * time.Hour,
-		principals:  []string{"ec2-user"},
-		permissions: []string{"permit-pty", "force-command=/bin/ls"},
-	}
+	signer *KeySigner
 )
+
+func TestMain(m *testing.M) {
+	f, _ := ioutil.TempFile("", "ca_key")
+	defer os.Remove(f.Name())
+	ioutil.WriteFile(f.Name(), testdata.Priv, 0600)
+	conf := &config.SSH{
+		SigningKey:           f.Name(),
+		MaxAge:               "1h",
+		AdditionalPrincipals: []string{"ec2-user"},
+		Permissions:          []string{"permit-pty", "force-command=/bin/ls"},
+	}
+	var err error
+	signer, err = New(conf)
+	if err != nil {
+		fmt.Printf("Unable to create signer: %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(m.Run())
+}
 
 func TestCert(t *testing.T) {
 	r := &lib.SignRequest{
@@ -98,6 +115,35 @@ func TestPermissions(t *testing.T) {
 	}{
 		extensions: map[string]string{"permit-pty": ""},
 		options:    map[string]string{"force-command": "/bin/ls"},
+	}
+	if !reflect.DeepEqual(cert.Extensions, want.extensions) {
+		t.Errorf("Wrong permissions: wanted: %v got :%v", cert.Extensions, want.extensions)
+	}
+	if !reflect.DeepEqual(cert.CriticalOptions, want.options) {
+		t.Errorf("Wrong options: wanted: %v got :%v", cert.CriticalOptions, want.options)
+	}
+}
+
+func TestDefaultPermissions(t *testing.T) {
+	r := &lib.SignRequest{
+		Key:        string(testdata.Pub),
+		ValidUntil: time.Now().Add(1 * time.Hour),
+	}
+	key, _ := ssh.ParsePrivateKey(testdata.Priv)
+	signer := &KeySigner{
+		ca:       key,
+		validity: 12 * time.Hour,
+	}
+	cert, err := signer.SignUserKey(r, "gopher1")
+	if err != nil {
+		t.Error(err)
+	}
+	want := struct {
+		extensions map[string]string
+		options    map[string]string
+	}{
+		extensions: defaultPermissions,
+		options:    map[string]string{},
 	}
 	if !reflect.DeepEqual(cert.Extensions, want.extensions) {
 		t.Errorf("Wrong permissions: wanted: %v got :%v", cert.Extensions, want.extensions)
